1550aa0fce9e0109f2543dd6b223440ac619af7a
Chaz Kondo <Chaz@Macbook-Pro-2.home>
2018-10-13 15:31:16 -1000

    Edit migrations and seeds

--- server/db/migrations/20181011152653_create_items_table.js
+++ server/db/migrations/20181011152653_create_items_table.js
@@ -1,17 +1,16 @@
-
 exports.up = function(knex, Promise) {
-    return knex.schema.createTable('items', (table) => {
-      table.increments();
-      table.string('name').notNullable();
-      table.integer('weight').notNullable();
-      table.string('type').notNullable();
-      table.timestamp('created_at').defaultTo(knex.fn.now())
-      table.timestamp('updated_at').defaultTo(knex.fn.now())
-      
-    }) 
-  };
-  
-  exports.down = function(knex, Promise) {
-    return knex.schema.dropTable('items')
-  };
-  
+  return knex.schema.createTable('items', (table) => {
+    table.increments('id').primary();
+    table.string('task').notNullable();
+    table.string('priority').notNullable();
+    table.string('description').notNullable();
+    table.string('type').notNullable();
+    table.timestamp('created_at').defaultTo(knex.fn.now())
+    table.timestamp('updated_at').defaultTo(knex.fn.now())
+    
+  }) 
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('items')
+};
