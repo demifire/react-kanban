1cdb09dc38e1392b5733b1865b02b65a641d06ac
Chaz Kondo <Chaz@Macbook-Pro-2.home>
2018-10-20 15:06:32 -1000

    Clean more

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,13 +1,13 @@
 import axios from 'axios';
 
-export const GET_ALL_ITEMS = 'GET_ALL_ITEMS'
+export const GET_ALL_ITEMS = 'GET_ALL_ITEMS';
 export const ADD_ITEM = 'ADD_ITEM';
 export const DELETE_ITEM_BY_ID = 'DELETE_ITEM_BY_ID';
 export const EDIT_ITEM_BY_ID = 'EDIT_ITEM_BY_ID';
 export const REORDER_ITEM = 'REORDER_ITEM';
 export const CHANGE_ITEM_TYPE = 'CHANGE_ITEM_TYPE';
-export const SET_VISIBLE_TO_TRUE = 'SET_VISIBLE_TO_TRUE'
-export const SET_VISIBLE_TO_FALSE = 'SET_VISIBLE_TO_FALSE'
+export const SET_VISIBLE_TO_TRUE = 'SET_VISIBLE_TO_TRUE';
+export const SET_VISIBLE_TO_FALSE = 'SET_VISIBLE_TO_FALSE';
 
 let cache;
 
@@ -22,57 +22,53 @@ export const getAllItems = () => {
                 dispatch({type: GET_ALL_ITEMS, payload: cache})
             })
             .catch( err => {
-                console.log(err, 'err')
+                // console.log(err, 'err')
             })
     }
-}
+};
 
 export const addItem = (item) => {
 
-    console.log('ACTION: addItem',  item);
     cache = [...cache, item];
-
     for ( let i = 0; i < cache.length; i++ ) {
         cache[i].sortingid = i+1;
-    }
+    };
 
     axios.post('/', item)
     .then( response => {
-      console.log('response', response.data);
+    //   console.log('response', response.data);
     })
     .catch( err => {
-      console.log('err in addItem action axios call', err)
+    //   console.log('err in addItem action axios call', err)
     });
     return dispatch => {
         dispatch({type: ADD_ITEM, payload: cache})
     }
-  }
+  };
 
   export const deleteItemByIdAction = (item, currentCache) => {
     
-    console.log('ACTION: Delete Item')
     const index = cache.findIndex(element => element.sortingid === item.sortingid);
     cache[index].type = false;
     cache = [...cache];
     for ( let i = 0; i < cache.length; i++ ) {
         cache[i].sortingid = i+1;
-    }
+    };
 
     axios.delete('/:id', { data: { item } })
         .then( response => {
-          console.log('DELETE response', response.data);
+        //   console.log('DELETE response', response.data);
         })
         .catch( err => {
-          console.log('err in delete action axios call', err)
+        //   console.log('err in delete action axios call', err)
         })
 
     return dispatch => {
         dispatch({type: DELETE_ITEM_BY_ID, payload: cache})
     }
-  }
+  };
 
   export const editItem = (item) => {
-    console.log('ACTION: Edit Item')
 
     const index = cache.findIndex(element => element.sortingid === item.sortingid);
     cache[index].task = item.task;
@@ -81,16 +77,16 @@ export const addItem = (item) => {
 
     axios.put('/:id', { item })
     .then( response => {
-      console.log('Edit response', response.data)
+    //   console.log('Edit response', response.data)
     })
     .catch( err => {
-      console.log('err in addItem action axios call', err)
+    //   console.log('err in addItem action axios call', err)
     })
 
     return dispatch => {
         dispatch({type: EDIT_ITEM_BY_ID, payload: cache})
     }
-  }
+  };
 
   export const reorderItem = ( result, list, startIndex, endIndex, destination, source, currentCache ) => {
 
@@ -98,22 +94,14 @@ export const addItem = (item) => {
     let temp = sameArr[endIndex].sortingid-1;
     let temp2 = sameArr[startIndex].sortingid-1;
     let tempId = sameArr[endIndex].sortingid;
-    let temp2Id = sameArr[startIndex].sortingid;
 
-    // Swap start id with end id
     sameArr[startIndex].sortingid = tempId; 
-
-    // Splice out the starting item from the array
     let [removed] = cache.splice(temp2, 1);
-
-    // Insert it @ temp
-    cache.splice(temp, 0, removed)
-
-    // Reindex array 
+    cache.splice(temp, 0, removed);
 
     for (let i = 0; i < cache.length; i++) {
       cache[i].sortingid = i+1
-    }
+    };
 
     axios.put('/save', { currentCache })
     .then( response => {
@@ -126,7 +114,7 @@ export const addItem = (item) => {
     return dispatch => {
         dispatch({type: REORDER_ITEM, payload: cache})
     }
-  }
+  };
   
   export const changeItemType = (result) => {
 
@@ -142,21 +130,21 @@ export const addItem = (item) => {
     })
     .catch( err => {
       console.log('err in addItem action axios call', err)
-    })
+    });
   
     return dispatch => {
         dispatch({type: CHANGE_ITEM_TYPE, payload: cache})
     } 
-  }
+  };
 
 export const setVisibleTrue = () => {
     return dispatch => {
         dispatch({type: SET_VISIBLE_TO_TRUE, payload: true } )
     } 
-}
+};
 
 export const setVisibleFalse = () => {
     return dispatch => {
         dispatch({type: SET_VISIBLE_TO_FALSE, payload: false } )
     } 
-}
+};
